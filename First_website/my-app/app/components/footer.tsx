c3167a3536bd155a785ee2589517c464178b393a
Haider <haiderali605@hotmail.com>
2023-02-10 17:33:34 +0530

    footer 3

--- First_website/my-app/app/components/footer.tsx
+++ First_website/my-app/app/components/footer.tsx
@@ -7,27 +7,28 @@ import logo from "../../public/logo.png"
 
 export default function Footer() {
   return (
-    <Box boxShadow={"lg"} bg={"yellow.200"} >
+    <Box boxShadow={"lg"} bg={"blackAlpha.300"} >
 
     <Flex  justifyContent='normal'>
-        <Box ml={"5"}  height={"200"} width={"30"} bg={"gray.100"}>
+        <Box ml={"5"}  height={"200"} width={"30"} >
     <Image src={logo} alt={"Panaverse-logo"} height={100} width={170} ></Image>
-    <Text>Certified Web 3.0 and Metaverse Developer:<br/> 
+    <Text fontWeight={"semibold"}>Certified Web 3.0 and Metaverse Developer:<br/> 
         A Nationwide Program in Karachi, Lahore, Islamabad, and Peshawar</Text>
         </Box>
-        <Flex bg={"aqua"}  m={"auto"}>
-        <VStack fontSize={18}  fontWeight='semibold' pl={"3"}  width={300}>
-            <Heading>Pages</Heading>
+        <Flex  ml={'200'}>
+        <VStack fontWeight={"normal"} pl={"3"} pt={"3"} 
+         width={300}>
+            <Heading fontSize={"22"} >Pages</Heading>
             <Link href={""} >Home</Link>
             <Link href={""}>Courses</Link>
             <Link href={""}>About</Link>
             <Link href={""}>Contact Us</Link>
             
         </VStack>
-        <VStack fontSize={18} ml={"4"} mr={'0'} fontWeight='semibold' 
+        <VStack fontWeight={"normal"}  ml={"4"} mr={'0'} pt={"3"} 
         pl={""}  width={300} 
-        placeContent={"center"}>
-            <Heading>Programs</Heading>
+        >
+            <Heading fontSize={"22"}>Programs</Heading>
             <Link href={""} >Home</Link>
             <Link href={""}>Courses</Link>
             <Link href={""}>About</Link>
